297e8593495ce18d4a000be752acfe1958380397
btheodorou <btheodorou@g.hmc.edu>
2022-02-17 13:47:32 -0600

    Fixed the ICFG graph format, enables whole directory

--- SootScripts/src/sootscripts/CallGraphScript.java
+++ SootScripts/src/sootscripts/CallGraphScript.java
@@ -21,17 +21,17 @@ public class CallGraphScript
 {	
 	public static void main(String[] args) {
 		// Soot classpath
-		String path = System.getProperty("user.dir")+"/src";
+		String path = System.getProperty("user.dir")+"/../auto/common/src";
 
 		// Setting the classpath programatically
 		Options.v().set_prepend_classpath(true);
 		Options.v().set_soot_classpath(path);
 
 		Options.v().set_allow_phantom_refs(true);
-//		Options.v().set_whole_program(true);
-//		Options.v().set_app(true);
+		Options.v().set_whole_program(true);
+		Options.v().set_app(true);
 
-		args = new String[]{"-w", "testers.ExampleCode"};
+		args = new String[]{"-w", "-process-dir", path};
 
 		PackManager.v().getPack("wjtp").add(new Transform("wjtp.myTrans", new SceneTransformer() {
 
--- SootScripts/src/sootscripts/ICFGScript.java
+++ SootScripts/src/sootscripts/ICFGScript.java
@@ -48,10 +48,18 @@ public class ICFGScript extends SceneTransformer {
                 Unit parent = nodes.remove(0);
                 List<Unit> targets = icfg.getSuccsOf(parent);
                 while (!targets.isEmpty()) {
-                    Unit child = targets.get(0);
-                    nodes.add(child);
-                    writer.write("\t\"" + parent.toString() + "\" -> \"" + child.toString()+ "\"\n");
-                    targets.remove(0);
+                    Unit child = targets.remove(0);
+                    if (child.toString().equals("nop") || child.toString().equals("goto [?= nop]")) {
+                        targets.addAll(icfg.getSuccsOf(child));
+                    }
+                    else {
+                        nodes.add(child);
+                        String parentString = parent.toString();
+                        String childString = child.toString();
+                        parentString = parentString.contains(" goto nop") ? parentString.substring(0, parentString.length() - 9) : parentString;
+                        childString = childString.contains(" goto nop") ? childString.substring(0, childString.length() - 9) : childString;
+                        writer.write("\t\"" + parentString + "\" -> \"" + childString + "\"\n");
+                    }
                 }
             }
             writer.write("}");
@@ -63,18 +71,16 @@ public class ICFGScript extends SceneTransformer {
 
     public static void main(String args[]) {
         // Soot classpath
-        String path = System.getProperty("user.dir")+"/src";//"/../commons-math/commons-math-core/src";
+        String path = System.getProperty("user.dir")+"/../auto/common/src";
 
         // Setting the classpath programatically
         Options.v().set_prepend_classpath(true);
         Options.v().set_soot_classpath(path);
 
-//        Options.v().set_output_format(Options.output_format_jimple);
-//        Options.v().set_output_dir(System.getProperty("user.dir")+"/output");
-
         // Enable whole-program mode
         Options.v().set_whole_program(true);
         Options.v().set_app(true);
+        Options.v().set_allow_phantom_refs(true);
 
         // Call-graph options
         Options.v().setPhaseOption("cg", "safe-newinstance:true");
@@ -85,23 +91,21 @@ public class ICFGScript extends SceneTransformer {
         Options.v().setPhaseOption("cg.spark","verbose:true");
         Options.v().setPhaseOption("cg.spark","on-fly-cg:true");
 
-        Options.v().set_allow_phantom_refs(true);
-
         // Set the main class of the application to be analysed
-        Options.v().set_main_class("testers.ExampleCode");
+//        Options.v().set_main_class("testers.ExampleCode");
 
         // Load the main class
-        SootClass c = Scene.v().loadClass("testers.ExampleCode", SootClass.BODIES);
-        c.setApplicationClass();
+//        SootClass c = Scene.v().loadClass("testers.ExampleCode", SootClass.BODIES);
+//        c.setApplicationClass();
 
         // Load the "main" method of the main class and set it as a Soot entry point
-        SootMethod entryPoint = c.getMethodByName("main");
-        List<SootMethod> entryPoints = new ArrayList<SootMethod>();
-        entryPoints.add(entryPoint);
-        Scene.v().setEntryPoints(entryPoints);
+//        SootMethod entryPoint = c.getMethodByName("main");
+//        List<SootMethod> entryPoints = new ArrayList<SootMethod>();
+//        entryPoints.add(entryPoint);
+//        Scene.v().setEntryPoints(entryPoints);
 
         PackManager.v().getPack("wjtp").add(new Transform("wjtp.herosifds", new ICFGScript()));
 
-        soot.Main.main(new String[]{"-w", "testers.ExampleCode", "-pp"});
+        soot.Main.main(new String[]{"-w", "-process-dir", path});
     }
 }
