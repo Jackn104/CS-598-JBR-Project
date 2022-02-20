3d7fe16dc67be108764589d109f6847ba470b941
btheodorou <btheodorou@g.hmc.edu>
2022-02-19 18:30:24 -0600

    Added updated scripts

--- SootScripts/src/sootscripts/CallGraphScript.java
+++ SootScripts/src/sootscripts/CallGraphScript.java
@@ -1,17 +1,10 @@
 package sootscripts;
 
 import soot.*;
-import soot.jimple.toolkits.callgraph.CHATransformer;
 import soot.jimple.toolkits.callgraph.CallGraph;
-import soot.jimple.toolkits.callgraph.Targets;
+import soot.jimple.toolkits.callgraph.Edge;
 import soot.options.Options;
-
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import soot.SceneTransformer;
 import soot.SootMethod;
 import java.io.File;
 import java.io.FileWriter;
@@ -26,62 +19,39 @@ public class CallGraphScript
 		// Setting the classpath programatically
 		Options.v().set_prepend_classpath(true);
 		Options.v().set_soot_classpath(path);
-
 		Options.v().set_allow_phantom_refs(true);
-		Options.v().set_whole_program(true);
-		Options.v().set_app(true);
+		args = new String[] {"-w", "-process-dir", path};
 
-		Scene.v().loadNecessaryClasses();
-		args = new String[]{"-w", "-process-dir", path};
-
-		PackManager.v().getPack("wjtp").add(new Transform("wjtp.myTrans", new SceneTransformer() {
-
-			@Override
-			protected void internalTransform(String phaseName, Map options) {
-				CHATransformer.v().transform();
-
-				// This line generates the call graph of the project you're going to analyze
-				CallGraph cg = Scene.v().getCallGraph();
-				// Now that you have the call graph, you can start doing whatever type of
-				// analysis needed for your problem. Below, we are going to traverse the
-				// generated call graph and print caller/callee relationships (BFS)
-
-				SootMethod src = Scene.v().getMainClass().getMethodByName("main");
-				List<SootMethod> nodes = new ArrayList<>();
-				nodes.add(src);
-
-				File file = new File("../GraphViz/DotFiles/CG.txt");
-				try {
-					file.delete();
-					file.createNewFile();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				FileWriter writer = null;
-				try {
-					writer = new FileWriter("../GraphViz/DotFiles/CG.txt");
-					writer.write("digraph {\n");
-					while(!nodes.isEmpty()){
-						SootMethod parent = nodes.get(0);
-						Iterator<MethodOrMethodContext> targets = new Targets(cg.edgesOutOf(parent));
-						while (targets.hasNext()) {
-							SootMethod child = (SootMethod) targets.next();
-							if(!child.getSignature().contains("init"))
-								nodes.add(child);
-							writer.write("\t\""+parent.getDeclaringClass()+"."+parent.getName()+
-									"\" -> \"" + child.getDeclaringClass()+"."+child.getName()+"\"\n");
-						}
-						nodes.remove(0);
-					}
-					writer.write("}");
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}));
-
+		System.out.println("Starting analysis");
 		Main.main(args);
+		System.out.println("Finished analysis");
+
+		CallGraph cg = Scene.v().getCallGraph();
+		System.out.println("Got Callgraph");
+
+		File file = new File("../GraphViz/DotFiles/CG.txt");
+		try {
+			file.delete();
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		FileWriter writer;
+		try {
+			writer = new FileWriter("../GraphViz/DotFiles/CG.txt");
+			writer.write("digraph {\n");
+			Iterator<Edge> edgeIterator = cg.iterator();
+			while(edgeIterator.hasNext()){
+				Edge edge = edgeIterator.next();
+				SootMethod src = edge.src();
+				SootMethod tgt = edge.tgt();
+				writer.write("\t\""+src.getDeclaringClass()+"."+src.getName()+
+							"\" -> \"" + tgt.getDeclaringClass()+"."+tgt.getName()+"\"\n");
+				}
+			writer.write("}");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- SootScripts/src/sootscripts/ICFGScript.java
+++ SootScripts/src/sootscripts/ICFGScript.java
@@ -1,10 +1,14 @@
 package sootscripts;
 
+import heros.DefaultSeeds;
 import heros.IFDSTabulationProblem;
 import heros.InterproceduralCFG;
 import heros.solver.IFDSSolver;
 import soot.*;
 import soot.jimple.DefinitionStmt;
+import soot.jimple.internal.JimpleLocal;
+import soot.jimple.toolkits.ide.JimpleIFDSSolver;
+import soot.jimple.toolkits.ide.exampleproblems.IFDSPossibleTypes;
 import soot.jimple.toolkits.ide.exampleproblems.IFDSReachingDefinitions;
 import soot.jimple.toolkits.ide.icfg.JimpleBasedInterproceduralCFG;
 import soot.toolkits.scalar.Pair;
@@ -15,97 +19,122 @@ import java.io.IOException;
 import java.util.*;
 
 // Subclass of SceneTransformer to run Heros IFDS solver in Soot's "wjtp" pack
-public class ICFGScript extends SceneTransformer {
-    @Override
-    protected void internalTransform(String phaseName, Map<String, String> options) {
-        JimpleBasedInterproceduralCFG icfg = new JimpleBasedInterproceduralCFG();
-        IFDSTabulationProblem<Unit, Pair<Value,
-                Set<DefinitionStmt>>, SootMethod,
-                InterproceduralCFG<Unit, SootMethod>> problem = new IFDSReachingDefinitions(icfg);
-
-        IFDSSolver<Unit, Pair<Value, Set<DefinitionStmt>>,
-                SootMethod, InterproceduralCFG<Unit, SootMethod>> solver =
-                new IFDSSolver<Unit, Pair<Value, Set<DefinitionStmt>>, SootMethod,
-                        InterproceduralCFG<Unit, SootMethod>>(problem);
-
-        solver.solve();
-
-        SootMethod src = Scene.v().getMainClass().getMethodByName("main");
-        List<Unit> nodes = (List) icfg.getStartPointsOf(src);
-
-        File file = new File("../GraphViz/DotFiles/ICFG.txt");
-        try {
-            file.delete();
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("../GraphViz/DotFiles/ICFG.txt");
-            writer.write("digraph {\n");
-            while(!nodes.isEmpty()){
-                Unit parent = nodes.remove(0);
-                List<Unit> targets = icfg.getSuccsOf(parent);
-                while (!targets.isEmpty()) {
-                    Unit child = targets.remove(0);
-                    if (child.toString().equals("nop") || child.toString().equals("goto [?= nop]")) {
-                        targets.addAll(icfg.getSuccsOf(child));
-                    }
-                    else {
-                        nodes.add(child);
-                        String parentString = parent.toString();
-                        String childString = child.toString();
-                        parentString = parentString.contains(" goto nop") ? parentString.substring(0, parentString.length() - 9) : parentString;
-                        childString = childString.contains(" goto nop") ? childString.substring(0, childString.length() - 9) : childString;
-                        writer.write("\t\"" + parentString + "\" -> \"" + childString + "\"\n");
-                    }
-                }
-            }
-            writer.write("}");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class ICFGScript
+{
+//    @Override
+//    protected void internalTransform(String phaseName, Map<String, String> options) {
+//        JimpleBasedInterproceduralCFG icfg = new JimpleBasedInterproceduralCFG();
+//
+//
+//        IFDSTabulationProblem<Unit, Pair<Value,
+//                Set<DefinitionStmt>>, SootMethod,
+//                InterproceduralCFG<Unit, SootMethod>> problem = new IFDSReachingDefinitions(icfg);
+//        System.out.println("yooooo got here3");
+//        IFDSSolver<Unit, Pair<Value, Set<DefinitionStmt>>,
+//                SootMethod, InterproceduralCFG<Unit, SootMethod>> solver =
+//                new IFDSSolver<Unit, Pair<Value, Set<DefinitionStmt>>, SootMethod,
+//                        InterproceduralCFG<Unit, SootMethod>>(problem);
+//        System.out.println("yooooo got here2");
+//        solver.solve();
+//
+//        System.out.println("yooooo got here");
+//        SootMethod src = Scene.v().getMainClass().getMethodByName("main");
+//        List<Unit> nodes = (List) icfg.getStartPointsOf(src);
+//
+//        File file = new File("../GraphViz/DotFiles/ICFG.txt");
+//        try {
+//            file.delete();
+//            file.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        FileWriter writer = null;
+//        try {
+//            writer = new FileWriter("../GraphViz/DotFiles/ICFG.txt");
+//            writer.write("digraph {\n");
+//            while(!nodes.isEmpty()){
+//                Unit parent = nodes.remove(0);
+//                List<Unit> targets = icfg.getSuccsOf(parent);
+//                while (!targets.isEmpty()) {
+//                    Unit child = targets.remove(0);
+//                    if (child.toString().equals("nop") || child.toString().equals("goto [?= nop]")) {
+//                        targets.addAll(icfg.getSuccsOf(child));
+//                    }
+//                    else {
+//                        nodes.add(child);
+//                        String parentString = parent.toString();
+//                        String childString = child.toString();
+//                        parentString = parentString.contains(" goto nop") ? parentString.substring(0, parentString.length() - 9) : parentString;
+//                        childString = childString.contains(" goto nop") ? childString.substring(0, childString.length() - 9) : childString;
+//                        writer.write("\t\"" + parentString + "\" -> \"" + childString + "\"\n");
+//                    }
+//                }
+//            }
+//            writer.write("}");
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void main(String args[]) {
         // Soot classpath
-        String path = System.getProperty("user.dir") + "/" + args[0];
+        String path = System.getProperty("user.dir") + "/" + "../auto/common/target/classes"; //args[0];
 
         // Setting the classpath programatically
         Options.v().set_prepend_classpath(true);
         Options.v().set_soot_classpath(path);
-
-        // Enable whole-program mode
-        Options.v().set_whole_program(true);
-        Options.v().set_app(true);
         Options.v().set_allow_phantom_refs(true);
 
-        // Call-graph options
-        Options.v().setPhaseOption("cg", "safe-newinstance:true");
-        Options.v().setPhaseOption("cg.cha","enabled:false");
+//        //Set entry points
+//        List<SootMethod> entryPoints = new ArrayList<SootMethod>();
+//        for (SootClass sc : Scene.v().getClasses()){
+//            System.out.println(sc.toString());
+//            if (sc.declaresMethodByName("main"))
+//                entryPoints.add(sc.getMethodByName("main"));
+//        }
+//
+//        Scene.v().setEntryPoints(entryPoints);
+//        System.out.println(entryPoints.size());
 
-        // Enable SPARK call-graph construction
-        Options.v().setPhaseOption("cg.spark","enabled:true");
-        Options.v().setPhaseOption("cg.spark","verbose:true");
-        Options.v().setPhaseOption("cg.spark","on-fly-cg:true");
+        // Call-graph options
+        Options.v().setPhaseOption("cg", "all-reachable:true");
+//        Options.v().setPhaseOption("cg.cha","enabled:false");
+//
+//        // Enable SPARK call-graph construction
+//        Options.v().setPhaseOption("cg.spark","enabled:true");
+//        Options.v().setPhaseOption("cg.spark","verbose:true");
+//        Options.v().setPhaseOption("cg.spark","on-fly-cg:true");
+        args = new String[] {"-w", "-process-dir", path};
+
+        PackManager.v().getPack("wjtp").add(new Transform("wjtp.ifds", new SceneTransformer() {
+            protected void internalTransform(String phaseName, @SuppressWarnings("rawtypes") Map options) {
+                JimpleBasedInterproceduralCFG icfg = new JimpleBasedInterproceduralCFG();
+
+                IFDSTabulationProblem<Unit, ?, SootMethod, InterproceduralCFG<Unit, SootMethod>> problem
+                        = new IFDSPossibleTypes(icfg);
+
+                @SuppressWarnings({ "rawtypes", "unchecked" })
+                JimpleIFDSSolver<?, InterproceduralCFG<Unit, SootMethod>> solver = new JimpleIFDSSolver(problem);
+                solver.solve();
+            }
+        }));
 
         // Set the main class of the application to be analysed
-//        Options.v().set_main_class("testers.ExampleCode");
-
-        // Load the main class
-//        SootClass c = Scene.v().loadClass("testers.ExampleCode", SootClass.BODIES);
+//        String cls = "com.google.auto.common.SimpleTypeAnnotationValue";
+//        Options.v().set_main_class(cls);
+//
+//        // Load the main class
+//        SootClass c = Scene.v().loadClass(cls, SootClass.BODIES);
 //        c.setApplicationClass();
-
-        // Load the "main" method of the main class and set it as a Soot entry point
-//        SootMethod entryPoint = c.getMethodByName("main");
+//
+//        // Load the "main" method of the main class and set it as a Soot entry point
+//        SootMethod entryPoint = c.getMethodByName("accept");
 //        List<SootMethod> entryPoints = new ArrayList<SootMethod>();
 //        entryPoints.add(entryPoint);
 //        Scene.v().setEntryPoints(entryPoints);
 
-        PackManager.v().getPack("wjtp").add(new Transform("wjtp.herosifds", new ICFGScript()));
-
-        soot.Main.main(new String[]{"-w", "-process-dir", path});
+//        PackManager.v().getPack("wjtp").add(new Transform("wjtp.herosifds", new ICFGScript()));
+        soot.Main.main(args);
     }
 }
